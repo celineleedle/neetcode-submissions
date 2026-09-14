2b13edc1d8dbd06927c9a314befde93e1bdf026f
Celine <celineleedle@gmail.com>
2026-09-14 15:08:09 -0400

    Add: find-k-closest-elements - submission-7

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-7.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l, r = 0, k
+        # window starts from 0 up to (not including) element at k
+
+        while r < len(arr):
+            # need to check if this new element is better than the leftmost of window
+            # since that would be what gets dropped
+            num = arr[r]
+            if abs(num - x) < abs(arr[l] - x):
+                # strictly less than - so in case of tie we ignore the bigger element
+                # shrink the window to include this new element
+                l += 1
+            elif num == arr[l]:
+                # if they're the same number we continue sliding
+                l += 1
+            else:
+                # if we reach here then the number is worse than that arr[l]
+                # everything past this would also be worse
+                break
+
+            # move onto next
+            r += 1
+            # print(arr[l:l+k])
+        return arr[l:l+k]
